fcdb64ba37009c38472666ba27652a8f64c177ec
noi4339@gmail.com <7895123Noi>
2022-01-27 22:37:28 +0700

    PT27/01/22

--- reg_std.php
+++ reg_std.php
@@ -9,6 +9,7 @@
 
     $faculty = "SELECT * FROM faculty ";
     $result2 = mysqli_query($conn, $faculty);
+
    
 ?>
 
@@ -22,6 +23,7 @@
     <title>Sign up - Student Page</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/css/bootstrap-select.min.css">
     <link rel="stylesheet" href="css/reg_std.css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@400&display=swap" rel="stylesheet">
 
@@ -90,18 +92,29 @@
 
             <div class="col-sm-10">
               <label for="skill_id" class="form-label">ความถนัด</label>
-              <select type="text"  name="skill_id" class="form-control" required>
-              <option value="">-กรุณาเลือกความถนัด 1-</option>
-                <?php foreach($result1 as $skill){?>
-              <option value="<?php echo $skill["skill_id"];?>">
-                <?php echo $skill["skill_id"]; ?>
-              </option>
-                <?php } ?>
+              
+              <select type="text" name="skill_id" class=" selectpicker col-sm-12" multiple required
+                      title="-กรุณาเลือกความถนัด-"
+                      data-container="body"
+                      data-max-options="5"
+                      data-max-options-text="เลือก 5 รายการครบแล้ว"
+                      data-live-search="true" 
+                      data-selected-text-format="count > 5" >
+              <optgroup label="Picnic" >
+                <option >Mustard</option>
+                <option>Ketchup</option>
+                <option>Relish</option>
+              </optgroup>
+              <optgroup label="Camping">
+                <option>Tent</option>
+                <option>Flashlight</option>
+                <option>Toilet Paper</option>
+              </optgroup>
               </select>
             </div>
 
             <div class="col-sm-10">
-              <select type="text"  name="skill_id" class="form-control" required>
+              <select type="text"  name="skill_id" class="form-control " required>
               <option value="">-กรุณาเลือกความถนัด 2-</option>
                 <?php foreach($result1 as $skill){?>
               <option value="<?php echo $skill["skill_id"];?>">
@@ -124,7 +137,7 @@
 
             <div class="col-sm-10">
               <label for="faculty_id" class="form-label">คณะ</label>
-              <select type="text" name="faculty_id" id="faculty_id" class="form-control" required>
+              <select type="text" name="faculty_id" id="faculty_id" class="form-select" required>
               <option select disable value="">-กรุณาเลือกคณะ-</option>
                 <?php foreach($result2 as $faculty){?>
               <option value="<?=$faculty["faculty_id"]?>">
@@ -136,7 +149,7 @@
 
             <div class="col-sm-10">
               <label for="department_id" class="form-label">สาขาวิชา</label>
-              <select type="text" class="form-control" name="department_id" id="department_id" required>
+              <select type="text" class="form-select" name="department_id" id="department_id" required>
               <option selected disabled>-กรุณาเลือกสาขาวิชา-</option>
               </select>
             </div>        
@@ -162,10 +175,10 @@
               <input type="text" class="form-control" name="age" placeholder="" required>
             </div>
 
-            <div class="col-sm-4">
+            <div class="col-sm-5">
               
               <label for="sex" class="form-label">เพศ</label>
-              <select name="sex" class="form-control" id="sex" required>
+              <select name="sex" class="form-select" id="sex" required>
               <option value="">-กรุณาเลือก-</option>
               <option value="ชาย">ชาย</option>
               <option value="หญิง">หญิง</option>
@@ -181,12 +194,11 @@
     </div>
     
 
-    
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/bootstrap-select.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
 
